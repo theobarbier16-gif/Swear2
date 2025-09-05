7674f2592f5b9a2e54baecbdb50ea9f6507d8aa3
theobarbier16-gif <theo.barbier16@gmail.com>
2025-09-05 22:03:02 +0200

    Updated ResultsStep.tsx

--- src/components/ResultsStep.tsx
+++ src/components/ResultsStep.tsx
@@ -137,7 +137,7 @@ const ResultsStep: React.FC<ResultsStepProps> = ({
             flex items-center justify-center px-8 py-4 text-white font-medium rounded-xl transition-all duration-200
             ${downloadInitiated 
               ? 'bg-white/90 text-vinted-500 scale-95' 
-              : 'bg-white text-vinted-600 hover:bg-white/90 hover:scale-105 shadow-xl'
+              : 'bg-white text-vinted-500 hover:bg-white/90 hover:scale-105 shadow-xl'
             }
           `}
         >
